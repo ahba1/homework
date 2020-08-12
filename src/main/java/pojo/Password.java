880c7c9a15e4adcd4808c0e00a111faf8be5c1ef
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-12 16:06:48 +0000

    Password.java

---
+++ src/main/java/pojo/Password.java
@@ -0,0 +1,18 @@
+package pojo;
+
+public class Password {
+    private String password;
+
+    public Password(){}
+
+    public Password(String s){this.password=s;}
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
