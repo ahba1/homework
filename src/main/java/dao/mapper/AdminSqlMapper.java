d1cbac2371e7ab167679fedca6cc329c44d82897
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-26 17:20:15 +0000

    Update AdminSqlMapper.java

--- src/main/java/dao/mapper/AdminSqlMapper.java
+++ src/main/java/dao/mapper/AdminSqlMapper.java
@@ -17,6 +17,11 @@ public interface AdminSqlMapper {
             sqlStatement = "select * from admin",
             resultType = "pojo.Admin")
     List<Admin> selectAll();
+    
+    @Sql(sqlProcess = "select",
+            sqlStatement = "select * from interviewer,recruitment where interviewer.position=recruitment.position",
+            resultType = "pojo.Interviewer")
+    List<Interviewer> screen(int id);
 
     @Sql(sqlProcess = "select",
             sqlStatement = "select * from admin where id = #{id}",
@@ -38,5 +43,24 @@ public interface AdminSqlMapper {
             resultType = "java.lang.Boolean")
     Boolean insertRegisterInfo(String name,String username,String password);
 
+    @Sql(sqlProcess = "insert",
+            sqlStatement = "insert into recruitment(re_id,position,start_date,end_date)values(#{id},#{position},#{sqlStartDate},#{sqlEndDate})",
+            resultType = "pojo.Recruitment")
+    Recruitment publishRecruitment(int id, int position, String sqlStartDate, String sqlEndDate);
+    
+     @Sql(sqlProcess = "insert",
+            sqlStatement = "insert into delivery_info(username,recruitment,status,info)values('#{interviewerUsername}',recruitment,isRecruited,'#{info}')",
+            resultType = "pojo.DeliveryInfo")
+    DeliveryInfo remark(String interviewerUsername, int recruitment, int isRecruited, String info);
+    
+    @Sql(sqlProcess = "select",
+            sqlStatement = "select * from recruitment where re_id=id",
+            resultType = "pojo.Recruitment")
+    Recruitment query(int id);
+
+    @Sql(sqlProcess = "select",
+            sqlStatement = "select * from recruitment where username='#{username}'",
+            resultType = "pojo.Interviewer")
+    Interviewer query(String username);
 
 }
