d023e116d277fe2ab4c53e360a210831b32a8c46
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:42:25 +0000

    Create Position_Number.java

---
+++ src/main/java/pojo/Position_Number.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Position_Number {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
