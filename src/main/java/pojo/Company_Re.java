f504d900500aa72afd4baf534b397f67016c632b
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 14:41:40 +0000

    Create Company_Re.java

---
+++ src/main/java/pojo/Company_Re.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Company_Re {
+    private int com_id;
+    private int re_id;
+
+    public int getCom_id() {
+        return com_id;
+    }
+
+    public void setCom_id(int com_id) {
+        this.com_id = com_id;
+    }
+
+    public int getRe_id() {
+        return re_id;
+    }
+
+    public void setRe_id(int re_id) {
+        this.re_id = re_id;
+    }
+}
