e50ab65b763f68adf516e118a6a9982a3c5ccfc1
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:44:28 +0000

    Create Recruitment_id.java

---
+++ src/main/java/pojo/Recruitment_id.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Recruitment_id {
+    private int re_id;
+
+    public int getRe_id() {
+        return re_id;
+    }
+
+    public void setRe_id(int re_id) {
+        this.re_id = re_id;
+    }
+}
