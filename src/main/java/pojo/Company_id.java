50d36dcba29932155cb2249f4799c5d18dc8c618
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:39:08 +0000

    Create Company_id.java

---
+++ src/main/java/pojo/Company_id.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Company_id {
+    private int com_id;
+
+    public int getCom_id() {
+        return com_id;
+    }
+
+    public void setCom_id(int com_id) {
+        this.com_id = com_id;
+    }
+}
