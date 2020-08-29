b8d747b149f345594a451724a9ffbcdaabf845d0
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 14:40:43 +0000

    Create Company.java

---
+++ src/main/java/pojo/Company.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Company {
+    private int com_id;
+    private String info;
+
+    public int getCom_id() {
+        return com_id;
+    }
+
+    public void setCom_id(int com_id) {
+        this.com_id = com_id;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
