922739536b3a28d61de0f45a3bc2a67aa5bfa592
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:40:24 +0000

    Create Interviewer_Pos.java

---
+++ src/main/java/pojo/Interviewer_Pos.java
@@ -0,0 +1,31 @@
+package pojo;
+
+public class Interviewer_Pos {
+    private String username;
+    private int position;
+    private String info;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
