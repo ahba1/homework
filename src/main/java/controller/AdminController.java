80da6fdc639000c90afb9115156177018ac7e47a
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:02:52 +0000

    Update AdminController.java

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,12 +1,12 @@
 package controller;
 
-import pojo.Interviewer;
-import pojo.Position;
-import pojo.Recruitment;
-import service.impl.ServiceManager;
 
+
+import pojo.*;
+import service.impl.ServiceManager;
 import java.sql.Date;
 import java.util.List;
+
 
 @BaseController(type = "Admin")
 public class AdminController {
@@ -16,25 +16,30 @@ public class AdminController {
                 .login(username, password);
     }
 
-    public boolean register(String name, String username, String password, String passwordAgain){
+   public boolean register(String name, String username, String password, String passwordAgain,int com_id){
         return password.equals(passwordAgain) &&ServiceManager.getAdminService()
-                .register(name, username, password);
+                .register(name, username, password,com_id);
     }
 
 //改
-    public boolean publishRecruitment(int position, int number, String startDate, String endDate){
+    public boolean publishRecruitment(int position, int number, String startDate, String endDate,int com_id){
         return ServiceManager.getAdminService()
-                .publishRecruitment(position, number, startDate, endDate);
+                .publishRecruitment(position, number, startDate, endDate,com_id);
+    }
+    
+    public List<Apply_Info> screen(int re_id){
+        return ServiceManager.getAdminService()
+                .screen(re_id);
+    }
+    
+    public List<Apply_Info> screen(){
+        return ServiceManager.getAdminService()
+                .screen();
     }
 
-    public List<Interviewer> screen(int id){
-        return ServiceManager.getAdminService()
-                .screen(id);
-    }
-
-    public void mark(String interviewerUsername, int recruitment, int isRecruited, String info){
-        return ServiceManager.getAdminService()
-                .hire_confirmed(interviewerUsername, recruitment, isRecruited);
+    public boolean mark(String interviewerUsername, int recruitment, int isRecruited,String des){
+       return ServiceManager.getAdminService()
+                .hire_confirmed(interviewerUsername, recruitment, isRecruited,des);
     }
 
     public Recruitment query(int id){
@@ -47,13 +52,33 @@ public class AdminController {
                 .query(username);
     }
     
-    public boolean deleteFull(int re_id){
-        return ServiceManager.getAdminService()
-                .deleteFull(re_id);
+    public List<Recruitment> re_all_screen(){
+        return ServiceManager.getAdminService().re_all_screen();
     }
+
+    public List<Interviewer_Pos> auto_select(int position){
+        return ServiceManager.getAdminService().auto_select(position);
+    }
+    
+    public Interviewer_Info query_interviewer(String username){
+        return ServiceManager.getAdminService()
+                .query_interviewer(username);
+    }
+    
+//     public boolean deleteFull(int re_id){
+//         return ServiceManager.getAdminService()
+//                 .deleteFull(re_id);
+//     }
 
     public boolean delete(int re_id){
         return ServiceManager.getAdminService()
                 .delete(re_id);
+    }
+        public boolean updateCompanyInfo(int com_id,String info){
+        return ServiceManager.getAdminService().updateCompanyInfo(com_id,info);
+    }
+
+    public int getCompanyId(String username){
+        return ServiceManager.getAdminService().getCompanyId(username);
     }
 }
