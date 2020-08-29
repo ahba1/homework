0f736fcdaa09d4e3d071dd2abf0ba70b657303ad
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:35:28 +0000

    Update Admin.java

--- src/main/java/pojo/Admin.java
+++ src/main/java/pojo/Admin.java
@@ -1,22 +1,29 @@
 package pojo;
 
+import java.util.PrimitiveIterator;
+
 /**
  * this is just a test pojo
  */
 public class Admin {
 
     private String username;
-
-    private String password;
-
     private String name;
+    private String password;
+    private int com_id;
+
+    public int getCom_id() {
+        return com_id;
+    }
+
+    public void setCom_id(int com_id) {
+        this.com_id = com_id;
+    }
+
+    public Admin(){}
 
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
@@ -25,6 +32,10 @@ public class Admin {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
