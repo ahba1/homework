042d040c57410f375d27aa4321ae2f7cecfd5e4d
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:37:44 +0000

    Create Apply_Info.java

---
+++ src/main/java/pojo/Apply_Info.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Apply_Info {
+    private String username;
+    private int re_id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRe_id() {
+        return re_id;
+    }
+
+    public void setRe_id(int re_id) {
+        this.re_id = re_id;
+    }
+}
