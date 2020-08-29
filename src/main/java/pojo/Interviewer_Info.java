117542b0b96c79d29d03da24a49406351fbb1e48
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:39:41 +0000

    Create Interviewer_Info.java

---
+++ src/main/java/pojo/Interviewer_Info.java
@@ -0,0 +1,40 @@
+package pojo;
+
+public class Interviewer_Info {
+    private String username;
+    private int re_id;
+    private int isRecruited;
+    private String description;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRe_id() {
+        return re_id;
+    }
+
+    public void setRe_id(int re_id) {
+        this.re_id = re_id;
+    }
+
+    public int getIsRecruited() {
+        return isRecruited;
+    }
+
+    public void setIsRecruited(int isRecruited) {
+        this.isRecruited = isRecruited;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
