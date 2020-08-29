b6419eb2004f54f96687070b8072973a161723fb
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-29 07:41:13 +0000

    Create isFull.java

---
+++ src/main/java/pojo/isFull.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class IsFull {
+    private int isfull;
+
+    public int getIsfull() {
+        return isfull;
+    }
+
+    public void setIsfull(int isfull) {
+        this.isfull = isfull;
+    }
+}
