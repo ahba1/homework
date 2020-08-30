a8e796be718db40061f98b1c05f593be0566e195
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-30 10:19:02 +0000

    Update InterviewerController.java

--- src/main/java/controller/InterviewerController.java
+++ src/main/java/controller/InterviewerController.java
@@ -11,44 +11,44 @@ import java.sql.Date;
 
 import java.util.List;
 
+/**
+ * created by caizhengheng on 2020/8/17
+ * @author caizhengheng
+ * @version 1.0
+ */
+
 @BaseController(type = "Interviewer")
 public class InterviewerController {
-
+//登录
     public boolean login(String username, String password){
         return ServiceManager.getInterviewerService()
                 .login(username, password);
     }
-
+//注册
     public boolean register(String name, String username, String password, String passwordAgain){
         return password.equals(passwordAgain)
                 &&ServiceManager.getInterviewerService()
                 .register(name, username, password);
     }
-
+//添加职位能力
     public  boolean addAbilityInfo(String username,int position,String info){
         return ServiceManager.getInterviewerService().addAbilityInfo(username,position,info);
     }
 
-    
+//添加面试请求    
     public boolean apply(int recruitment_id, String username){
         return ServiceManager.getInterviewerService()
                 .apply(recruitment_id, username);
     }
     
     
-
+//按职位查询所有招聘信息id
     public List<Recruitment> query(int position){
         return ServiceManager
                 .getInterviewerService()
                 .query(position);
     }
-
-    public List<Recruitment> query(int position, String startDate, String endDate){
-        return ServiceManager
-                .getInterviewerService()
-                .query(position, startDate, endDate);
-    }
-    
+//显示录用信息  
     public Interviewer_Info show_status(String username){
         return ServiceManager.getInterviewerService().show_status(username);
     }
