ab716e88c915b4cb31812cfcc46d13cbc4efaa84
MidoriaEMM <60926028+MidoriaEMM@users.noreply.github.com>
2020-08-30 10:13:58 +0000

    Update ControllerManager.java

--- src/main/java/controller/ControllerManager.java
+++ src/main/java/controller/ControllerManager.java
@@ -1,5 +1,11 @@
 package controller;
 
+/**
+ * created by caizhengheng on 2020/8/17
+ * @author caizhengheng
+ * @version 1.0
+ */
+
 public class ControllerManager {
 
     private static AdminController adminController = new AdminController();
